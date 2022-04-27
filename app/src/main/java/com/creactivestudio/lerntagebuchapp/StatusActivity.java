dfbc04850305fc433a91e4036af0088a30c150ee
bolatSeref <bolat.seref.de@gmail.com>
2022-04-27 15:55:41 +0200

    timer is added

--- app/src/main/java/com/creactivestudio/lerntagebuchapp/StatusActivity.java
+++ app/src/main/java/com/creactivestudio/lerntagebuchapp/StatusActivity.java
@@ -4,21 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StatusActivity extends AppCompatActivity {
 
+    long startTime=0;
+    TextView tvTimer;
+
+    Handler timerHandler=new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+
+            tvTimer.setText(String.format("%d:%02d", minutes, seconds));
+
+            timerHandler.postDelayed(this, 500);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
         initViews();
+
+        startTime=System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable,0);
+
+    }
+
+    public void stopTimer (View view)
+    {
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
+    public void startTimer (View view)
+    {
+        startTime=System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable,0);
+    }
+
+    public void pausiereTimer (View view)
+    {
+
     }
 
     public void initViews()
     {
-
+        tvTimer=findViewById(R.id.tvTimer);
     }
 
     public void imgDoNotDisturbClicked (View view)
@@ -31,4 +71,11 @@ public class StatusActivity extends AppCompatActivity {
         Intent intent =new Intent(StatusActivity.this, NotizenActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        timerHandler.removeCallbacks(timerRunnable);
+    }
+
 }
